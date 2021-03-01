16809f6d39c5510bd3677dc30ad7a2d5c68c8ddc
Yorick Cleerbout <yorick.cleerbout@outlook.com>
2021-03-01 12:19:18 +0100

    Add files via upload

--- mongoDB.py
+++ mongoDB.py
@@ -1,4 +1,5 @@
 import pymongo as mongo
+import json
 
 
 def create_connection_to_collection():
@@ -8,5 +9,7 @@ def create_connection_to_collection():
 
 
 def save_to_mongo(data):
+    json_df = data.to_json(orient='records')
+    to_save = json.loads(json_df.replace('[', '').replace(']', ''))
     col = create_connection_to_collection()
-    col.insert_one(data)
+    col.insert_one(to_save)
