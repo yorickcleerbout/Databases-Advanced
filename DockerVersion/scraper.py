b4c9be1c2181dd11a8066af108ecf3b1d342091f
Yorick Cleerbout <yorick.cleerbout@outlook.com>
2021-03-19 15:17:17 +0100

    Update scraper.py

--- DockerVersion/scraper.py
+++ DockerVersion/scraper.py
@@ -6,7 +6,7 @@ import redis
 
 
 def cache_df(data):
-    r = redis.Redis(host='localhost', port=6379, db=0)
+    r = redis.Redis(host='redis', port=6379, db=0)
     r.setex('df', 60, data.to_json())
 
 
